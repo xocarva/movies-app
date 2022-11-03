d417733be31ad453c6f1f275f6420fa3cbe865e7
xocarva <correo@xoancarneiro.com>
2022-11-03 18:54:22 +0100

    update folder structure

--- src/Movies/pages/index.ts
+++ src/Movies/pages/index.ts
@@ -1,2 +1,3 @@
 
+export * from './FavsPage';
 export * from './MoviesPage';
--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-import { MoviesPage } from '../Movies/pages';
+import { FavsPage, MoviesPage } from '../Movies/pages';
 import { OopsPage } from '../pages';
-import { FavsPage } from '../Favs/pages/FavsPage';
 
 export const AppRouter = () => {
   return (
