6c0c7e752c65373cad1f6fad7c189c3cbe90e8ff
xocarva <correo@xoancarneiro.com>
2022-11-07 10:21:09 +0100

    update env variables

--- src/store/api/moviesAPI.ts
+++ src/store/api/moviesAPI.ts
@@ -1,7 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
-
 export const moviesAPI: AxiosInstance = axios.create({
-  baseURL: API_BASE_URL
+  baseURL: 'https://api.themoviedb.org/3/'
 });
--- src/store/slices/movies/thunks.ts
+++ src/store/slices/movies/thunks.ts
@@ -3,10 +3,10 @@ import { moviesAPI } from '../../api';
 import { mapFromApiToMovie } from '../../../utils';
 import { ResponseFromMovieDBApi } from '../../../types';
 
-const QUERY_BASE_URL = process.env.REACT_APP_QUERY_BASE_URL
+const API_KEY = process.env.REACT_APP_MOVIEDB_API_KEY;
 
 export const fetchMoviesByText = createAsyncThunk('movies/fetchByText', async (query: string) => {
-    const { data } = await moviesAPI.get<ResponseFromMovieDBApi>(`${QUERY_BASE_URL}${query}`);
+    const { data } = await moviesAPI.get<ResponseFromMovieDBApi>(`search/movie?api_key=${API_KEY}&query=${query}`);
     const movies = mapFromApiToMovie(data);
     const totalPages = data.total_pages;
     const page = data.page;
@@ -14,5 +14,4 @@ export const fetchMoviesByText = createAsyncThunk('movies/fetchByText', async (q
 
     return {movies, page, totalPages, totalResults};
   }
-
 );
