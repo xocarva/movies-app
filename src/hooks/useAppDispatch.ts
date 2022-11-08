856abbfb3defe52e538157692efc3b0f934c13d8
xocarva <correo@xoancarneiro.com>
2022-11-08 21:59:20 +0100

    fix comment typo

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -2,4 +2,4 @@ import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
-// necesasry for better typescript redux management (from docs)
+// necessary for better typescript redux management (from docs)
